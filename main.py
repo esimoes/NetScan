da1e33e8e7b08c19b16c802ad221e448bfe7a4ba
esimoes <emilianosimoes@gmail.com>
2024-07-16 20:17:17 -0300

    add scan basic

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import scapy.all as scapy
 
+def scan(ip):
+    arp_req_frame = scapy.ARP(pdst = ip)
 
+    broadcast_ether_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    
+    broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
 
+    answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
+    result = []
+    for i in range(0,len(answered_list)):
+        client_dict = {"ip" : answered_list[i][1].psrc, "mac" : answered_list[i][1].hwsrc}
+        result.append(client_dict)
 
-
+    return result
 if __name__=="__main__":
-    pass
+    print(scan("192.168.100.1"))
