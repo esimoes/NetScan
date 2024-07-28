5129c5ab99de099885c7e054f962eddd6488db61
esimoes <emilianosimoes@gmail.com>
2024-07-28 12:01:59 -0300

    working scan

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-import scapy.all as scapy
+import asyncio
+from scapy.all import IP, ICMP, sr1
+import ipaddress
+from ipaddress import IPv4Network
 
 from utils.ipcfg import get_ipconfig_data
 
-def scan_net(ip_range):
-    try:
-        ip = scapy.IP(dst=ip_range)
-        for p in ip:
-            result = scapy.send(p/scapy.ICMP(), verbose=False)
-            print(result)
-    except PermissionError as e:
-        print(f"Permission error: {e}. Try running the script as an administrator.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+def get_network_address(ip, netmask):
+    # Crear un objeto IPv4Network usando la dirección IP y la máscara de red
+    network = ipaddress.IPv4Network(f'{ip}/{netmask}', strict=False)
+    return network.with_prefixlen
+
+async def ping(ip):
+    ip_str = str(ip)
+    print(f"Pinging.. {ip_str}")
+    pkt = IP(dst=ip_str) / ICMP()
+    response = sr1(pkt, timeout=1, verbose=0)
+    if response:
+        print(f"{ip_str} is up")
+    else:
+        print(f"{ip_str} is down")
+
+async def scan_net(ip_range):
+    tasks = [ping(ip) for ip in ip_range]
+    await asyncio.gather(*tasks)
 
 def parse_ip_netmask(ip_address, netmask): # devuelve ip + mask format 192.168.1.0/24
     if "/" in netmask:
@@ -23,25 +34,11 @@ def parse_ip_netmask(ip_address, netmask): # devuelve ip + mask format 192.168.1
     
     return ip_with_mask
 
-def scan(ip):
-    arp_req_frame = scapy.ARP(pdst = ip)
-
-    broadcast_ether_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
-    
-    broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
-
-    answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
-    result = []
-    for i in range(0,len(answered_list)):
-        client_dict = {"ip" : answered_list[i][1].psrc, "mac" : answered_list[i][1].hwsrc}
-        result.append(client_dict)
-
-    return result
-
 if __name__=="__main__":
-    ip_dict = get_ipconfig_data()
-    ip_device = next(iter(ip_dict))
-    ip_dict = ip_dict[ip_device]
-    ip = parse_ip_netmask(ip_dict["inet4"],ip_dict["netmask"])
-
-    scan_net(ip)
+    ip_dict = get_ipconfig_data() #obtengo las ip del sistema
+    ip_device = next(iter(ip_dict)) #me quedo con la del primer adaptador
+    ip_dict = ip_dict[ip_device] #guardo la del primer adaptador en otro dict
+    ip_net = get_network_address(ip_dict["inet4"],ip_dict["netmask"]) #obtengo la ip de red
+    ip_net = IPv4Network(ip_net)
+    ip_range = ip_net.hosts()
+    asyncio.run(scan_net(ip_range))
--- utils/ipcfg.py
+++ utils/ipcfg.py
@@ -1,9 +1,48 @@
 import subprocess
 import re
+import locale
+
+def get_regex_lng():
+    lang = locale.getdefaultlocale()
+    encoder = lang[1]
+    try:
+        result = subprocess.run(
+            ["powershell", "-Command", "[CultureInfo]::InstalledUICulture.Name"],
+            capture_output=True,
+            text=True
+        )
+        cmd_language = result.stdout.strip()
+        print(f"Idioma de la consola: {cmd_language}")
+    except Exception as e:
+        print(f"No se pudo obtener el idioma de la consola: {e}")
+
+    regex = ""
+    if "en" in cmd_language:
+        regex = {
+        'dns_suffix': re.compile(r'\s*Connection-specific DNS Suffix  \. :\s*(\S*)'),
+        'inet6': re.compile(r'\s*IPv6 Address. . . . . . . . . . . : ([ABCDEFabcdef\d\:\%]+)'),
+        'inet4': re.compile(r'\s*IPv4 Address. . . . . . . . . . . : ([^\s\(]+)'),
+        'netmask': re.compile(r'\s*Subnet Mask . . . . . . . . . . . : ([^\s\(]+)'),
+        'gateway': re.compile(r'\s*Default Gateway . . . . . . . . . : ([^\s\(]+)')
+        }
+        adapter = "adapter"
+    elif "es" in cmd_language:
+        regex = {
+        'dns_suffix': re.compile(r'\s*Sufijo DNS específico para la conexión\. \. :\s*(\S*)'),
+        'inet6': re.compile(r'\s*Vínculo: dirección IPv6 local. . . : ([ABCDEFabcdef\d\:\%]+)'),
+        'inet4': re.compile(r'\s*Dirección IPv4. . . . . . . . . . . . . . : ([^\s\(]+)'),
+        'netmask': re.compile(r'\s*Máscara de subred . . . . . . . . . . . . : ([^\s\(]+)'),
+        'gateway': re.compile(r'\s*Puerta de enlace predeterminada . . . . . : ([^\s\(]+)')
+        }
+        adapter = 'Adaptador'
+    return regex, adapter, encoder
 
 def get_ipconfig_data():
+    # Expresiones regulares para extraer la información
+    regexes, adapter, encoder = get_regex_lng()
+
     # Ejecuta el comando ipconfig y obtiene la salida
-    result = subprocess.run(['ipconfig'], capture_output=True, text=True, encoding='cp850')
+    result = subprocess.run(['ipconfig'], capture_output=True, text=True, encoding=encoder)
     
     # Divide la salida en líneas
     output = result.stdout.splitlines()
@@ -11,22 +50,13 @@ def get_ipconfig_data():
     # Diccionario para almacenar los datos
     ipconfig_data = {}
     
-    # Expresiones regulares para extraer la información
-    regexes = {
-        'dns_suffix': re.compile(r'\s*Sufijo DNS específico para la conexión\. \. :\s*(\S*)'),
-        'inet6': re.compile(r'\s*Vínculo: dirección IPv6 local. . . : ([ABCDEFabcdef\d\:\%]+)'),
-        'inet4': re.compile(r'\s*Dirección IPv4. . . . . . . . . . . . . . : ([^\s\(]+)'),
-        'netmask': re.compile(r'\s*Máscara de subred . . . . . . . . . . . . : ([^\s\(]+)'),
-        'gateway': re.compile(r'\s*Puerta de enlace predeterminada . . . . . : ([^\s\(]+)')
-    }
-    
     # Variables temporales para almacenar los datos de cada adaptador
     current_adapter = None
     current_data = {}
     
     for line in output:
         # Si encuentra el nombre de un adaptador, guarda los datos del adaptador anterior y reinicia las variables
-        if 'Adaptador' in line:
+        if adapter in line:
             if current_adapter:
                 ipconfig_data[current_adapter] = current_data
             current_adapter = line.strip().replace('Adaptador de ', '')
